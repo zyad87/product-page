79cb19818cf3c51472111eddc8966d34cf684a00
zyad87 <zyadt871@gmail.com>
2023-09-08 04:53:18 +0300

    العمل على تجاوبية الصفحة

--- js/index.js
+++ js/index.js
@@ -14,3 +14,27 @@ function playvid(){
         icon.classList.add("fa-play");
     }
 }
+document.getElementById("fullyear").innerHTML= new Date().getFullYear();
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
+
+
